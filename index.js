15d04152c0ae4172976d97e37b7a4c3fbd6b41dc
marikadu <marika.duhhanina@student.hamk.fi>
2024-04-10 10:18:45 +0300

    fixed a little bit but still not working and is empty

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ require('dotenv').config();
 const app = express();
 
 // mongodb credentials
-// const dbURI = 'mongodb+srv://'+process.env.DBUSERNAME+':'
-// +process.env.DBPASSWORD+'@'+process.env.CLUSTER+'.mongodb.net/'
-// +process.env.DB+'?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://'+process.env.DBUSERNAME+':'
++process.env.DBPASSWORD+'@'+process.env.CLUSTER+'.mongodb.net/'
++process.env.DB+'?retryWrites=true&w=majority';
+
+
+mongoose.connect(dbURI)
+.then((result) => 
+{
+    console.log('Connected to DB');
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log("Listening on " + PORT));
+})
+.catch((err) => {
+    console.log(err);
+})
