494776f2bf339c1d377cd87865bbccb49e8dc193
Edem <edemq123@gmail.com>
2024-04-11 10:06:23 +0300

    Used handlebars to render index and admin pages. Connected styles.css file in public folder.

--- index.js
+++ index.js
@@ -16,10 +16,21 @@ app.get("/", (req, res) => {
   });
 });
 
-// Testing Port
-// app.get("/", (req, res) => {
-//   res.send("Testing");
-// });
+// Render the homepage to the browser (Also set the title for that page)
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Sunny Side Sandcastle",
+  });
+});
+
+// Rendering the admin.handlebars file (Serve as a template for other pages)
+app.get("/adminpage", (req, res) => {
+  res.render("admin", {
+    title: "Admin Secret Page",
+    // ADD OTHER ITEMS HERE FROM THE DATABASE. (products)
+  });
+});
+
 // Set the folder for static files (css, jpg)
 app.use(express.static("public"));
 const PORT = process.env.PORT || 5000; // Initializing the port to be used
