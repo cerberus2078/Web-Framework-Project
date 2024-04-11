288ac7e9525c348274ad9c907a90b542e6ef8981
Edem <edemq123@gmail.com>
2024-04-11 10:39:56 +0300

    handlebars setup completed. Included 404 status message and page

--- index.js
+++ index.js
@@ -12,26 +12,35 @@ app.set("view engine", "handlebars"); // Use handlebars as a template engine
 // Render the homepage to the browser (Also set the title for that page)
 app.get("/", (req, res) => {
   res.render("index", {
-    title: "Sunny Side Sandcastle",
+    title: "Home",
   });
 });
 
 // Render the homepage to the browser (Also set the title for that page)
 app.get("/", (req, res) => {
   res.render("index", {
-    title: "Sunny Side Sandcastle",
+    title: "Home",
+    companyName: "Sunny Side Sandcastle",
   });
 });
 
 // Rendering the admin.handlebars file (Serve as a template for other pages)
 app.get("/adminpage", (req, res) => {
   res.render("admin", {
-    title: "Admin Secret Page",
+    title: "Admin",
+    companyName: "Sunny Side Sandcastle",
     // ADD OTHER ITEMS HERE FROM THE DATABASE. (products)
   });
 });
 
 // Set the folder for static files (css, jpg)
 app.use(express.static("public"));
+
+// When no page has been found (404 error)
+app.use((req, res, next) => {
+  res.status(404).render("404", {
+    title: "404 Error",
+  });
+});
 const PORT = process.env.PORT || 5000; // Initializing the port to be used
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); // Confirmation of server running succcessfully
