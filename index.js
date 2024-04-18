305441ffbd7a4fca65f9963f19fe2dceed1d05b7
Edem <edemq123@gmail.com>
2024-04-18 13:31:53 +0300

    Progress
    Random updates

--- index.js
+++ index.js
@@ -4,25 +4,25 @@ const mongoose = require("mongoose");
 require("dotenv").config(); // Import the dotenv
 
 const app = express(); // Start the server by Creating the express module
-
+app.use("", require("./routes/users"));
 app.use(express.urlencoded({ extended: false })); // Get the data of the form to be able to pass and use it
 
-// Connect to the mongodb database
-const dbURI = `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@${process.env.CLUSTER}.mongodb.net/${process.env.DB}?retryWrites=true&w=majority&appName=Cluster0`;
+// // Connect to the mongodb database
+// const dbURI = `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@${process.env.CLUSTER}.mongodb.net/${process.env.DB}?retryWrites=true&w=majority&appName=Cluster0`;
 
-mongoose
-  .connect(dbURI)
-  .then((result) => {
-    const PORT = process.env.PORT || 5000; // Initializing the port to be used
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); // Confirmation of server running succcessfully
-    console.log("Connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// mongoose
+//   .connect(dbURI)
+//   .then((result) => {
+//     const PORT = process.env.PORT || 5000; // Initializing the port to be used
+//     app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); // Confirmation of server running succcessfully
+//     console.log("Connected to DB");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 // Import the User schema
-const User = require("./models/User");
+// const User = require("./models/User");
 
 // USER CRUD FUNCTIONS
 // User.createNewUser(
@@ -46,68 +46,68 @@ app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
 // Use handlebars as a template engine
 app.set("view engine", "handlebars"); // Use handlebars as a template engine
 
-// Render the homepage to the browser (Also set the title for that page)
-app.get("/", (req, res) => {
-  res.render("index", {
-    title: "Home",
-    companyName: "Sunny Side Sandcastle",
-  });
-});
-
-// I tried to fix the images but it didn't work
+// // Render the homepage to the browser (Also set the title for that page)
 // app.get("/", (req, res) => {
-//   imageList = [];
-//   imageList.push({ src: "logo2.png", name: "logo2" });
 //   res.render("index", {
-//     imageList: imageList,
 //     title: "Home",
 //     companyName: "Sunny Side Sandcastle",
 //   });
 // });
 
-// Rendering the admin.handlebars file (Serve as a template for other pages)
-app.get("/adminpage", (req, res) => {
-  res.render("admin", {
-    title: "Admin",
-    companyName: "Sunny Side Sandcastle",
-    // ADD OTHER ITEMS HERE FROM THE DATABASE. (products)
-  });
-});
+// // I tried to fix the images but it didn't work
+// // app.get("/", (req, res) => {
+// //   imageList = [];
+// //   imageList.push({ src: "logo2.png", name: "logo2" });
+// //   res.render("index", {
+// //     imageList: imageList,
+// //     title: "Home",
+// //     companyName: "Sunny Side Sandcastle",
+// //   });
+// // });
 
-// admin edit users page
-app.get("/admin-edit-user", (req, res) => {
-  res.render("adminedit", {
-    title: "Admin Edit User",
-    companyName: "Sunny Side Sandcastle",
-    // ADD OTHER ITEMS HERE FROM THE DATABASE. (products)
-  });
-});
+// // Rendering the admin.handlebars file (Serve as a template for other pages)
+// app.get("/adminpage", (req, res) => {
+//   res.render("admin", {
+//     title: "Admin",
+//     companyName: "Sunny Side Sandcastle",
+//     // ADD OTHER ITEMS HERE FROM THE DATABASE. (products)
+//   });
+// });
 
-// Adding users to the adminpage
-app.get("/users", async (req, res) => {
-  try {
-    const user = await User.find();
-    res.json(user);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
+// // admin edit users page
+// app.get("/admin-edit-user", (req, res) => {
+//   res.render("adminedit", {
+//     title: "Admin Edit User",
+//     companyName: "Sunny Side Sandcastle",
+//     // ADD OTHER ITEMS HERE FROM THE DATABASE. (products)
+//   });
+// });
 
-// Get booking page information where the user puts their own information (CRUD -> CREATE)
-app.get("/booking", (req, res) => {
-  res.render("booking", {
-    title: "Booking",
-    companyName: "SSS",
-  });
-});
+// // Adding users to the adminpage
+// app.get("/users", async (req, res) => {
+//   try {
+//     const user = await User.find();
+//     res.json(user);
+//   } catch (err) {
+//     res.status(500).send(err);
+//   }
+// });
 
-// Route for thank-you page
-app.get("/thank-you", (req, res) => {
-  res.render("thank-you", {
-    title: "Thank You",
-    companyName: "Sunny Side Sandcastle",
-  });
-});
+// // Get booking page information where the user puts their own information (CRUD -> CREATE)
+// app.get("/booking", (req, res) => {
+//   res.render("booking", {
+//     title: "Booking",
+//     companyName: "SSS",
+//   });
+// });
+
+// // Route for thank-you page
+// app.get("/thank-you", (req, res) => {
+//   res.render("thank-you", {
+//     title: "Thank You",
+//     companyName: "Sunny Side Sandcastle",
+//   });
+// });
 
 // Route for creating the user and sending them to a thank you page where they can still edit/delete their information (if we have time to implement it)
 app.post("/users", async (req, res) => {
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,108 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 // Creating a 'users' module for hanlding routes for Users.
+
+// Connect to the mongodb database
+const dbURI = `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@${process.env.CLUSTER}.mongodb.net/${process.env.DB}?retryWrites=true&w=majority&appName=Cluster0`;
+
+// mongoose
+//   .connect(dbURI)
+//   .then((result) => {
+//     const PORT = process.env.PORT || 5000; // Initializing the port to be used
+//     app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); // Confirmation of server running succcessfully
+//     console.log("Connected to DB");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+mongoose.connect(dbURI)
+
+  // Import the User schema
+const User = require("../models/User");
+
+// TESTING ROUTER WITH DATABASE
+
+
+
+
+router.get("/getone/:id", async (req, res) => {
+    const id = req.params.id
+    try {
+      const user = await User.findOne({ userID: id });
+      // res.json(user);
+      console.log(user); // Change to send the data to the page
+    } catch (error) {
+      console.log(error);
+    
+  };
+
+// Render the homepage to the browser (Also set the title for that page)
+router.get("/", (req, res) => {
+  res.render("index", {
+    title: "Home",
+    companyName: "Sunny Side Sandcastle",
+  });
+});
+
+// I tried to fix the images but it didn't work
+// app.get("/", (req, res) => {
+//   imageList = [];
+//   imageList.push({ src: "logo2.png", name: "logo2" });
+//   res.render("index", {
+//     imageList: imageList,
+//     title: "Home",
+//     companyName: "Sunny Side Sandcastle",
+//   });
+// });
+
+// Rendering the admin.handlebars file (Serve as a template for other pages)
+router.get("/adminpage", (req, res) => {
+  res.render("admin", {
+    title: "Admin",
+    companyName: "Sunny Side Sandcastle",
+    // ADD OTHER ITEMS HERE FROM THE DATABASE. (products)
+  });
+});
+
+// admin edit users page
+router.get("/admin-edit-user", (req, res) => {
+  res.render("adminedit", {
+    title: "Admin Edit User",
+    companyName: "Sunny Side Sandcastle",
+    // ADD OTHER ITEMS HERE FROM THE DATABASE. (products)
+  });
+});
+
+// Adding users to the adminpage
+router.get("/users", async (req, res) => {
+  try {
+    const user = await User.find();
+    res.json(user);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+// Get booking page information where the user puts their own information (CRUD -> CREATE)
+router.get("/booking", (req, res) => {
+  res.render("booking", {
+    title: "Booking",
+    companyName: "SSS",
+  });
+});
+
+// Route for thank-you page
+router.get("/thank-you", (req, res) => {
+  res.render("thank-you", {
+    title: "Thank You",
+    companyName: "Sunny Side Sandcastle",
+  });
+  router;
+});
+
+// Export the router
+module.exports = router;
