12c0a9c6ffefb278e41e678e2c9630646aed1a16
Edem <edemq123@gmail.com>
2024-04-22 14:24:37 +0300

    Commenced work on Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -1,75 +1,100 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const userSchema = new mongoose.Schema({
-//   userID: {
-//     type: Number,
-//     required: true,
-//   },
-//   firstName: {
-//     type: String,
-//     required: true,
-//   },
-//   lastName: {
-//     type: String,
-//     required: true,
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//   },
-//   phoneNumber: {
-//     type: String,
-//     required: true,
-//   },
-// });
+const userSchema = new mongoose.Schema({
+  userID: {
+    type: Number,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  // roomNumber: {
+  //  type: String,
+  //  required: false,
+  // },
+  // roomNumber: {
+  //   type: String,
+  //   required: false,
+  // },
+  // bookingDate: {
+  //   type: String,
+  //   required: false,
+  // },
+});
 
-// const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
-// // Database Functions Here (CRUD)
-// // GET ALL ITEMS IN THE DATABASE
-// const getAll = async () => {
-//   try {
-//     const result = await User.find();
-//     console.log(result);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
+// Database Functions Here (CRUD)
 
-// // CREATE ONE BY USER_ID FUNCTION
+// CREATE ONE BY USER_ID FUNCTION
 
-// const createNewUser = async (
-//   userID,
-//   firstName,
-//   lastName,
-//   email,
-//   phoneNumber
-// ) => {
-//   const newUser = new User({
-//     userID: userID,
-//     firstName: firstName,
-//     lastName: lastName,
-//     email: email,
-//     phoneNumber: phoneNumber,
-//   });
-//   newUser.save().then((result) => {
-//     console.log(result);
-//   });
-// };
+const createNewUser = (userData) => {
+  try {
+    const newUser = new User(userData);
+    newUser.save();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-// // READ ONE BY userID FUNCTION
-// const getOneUser = async (id) => {
-//   try {
-//     const user = await User.findOne({ userID: id });
-//     // res.json(user);
-//     console.log(user); // Change to send the data to the page
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
+// GET ALL ITEMS IN THE DATABASE
+const getAllUsers = async () => {
+  try {
+    const users = await User.find().lean();
+    return users;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-// // UPDATE ONE BY USER_ID function
+// READ ONE BY userID FUNCTION
+const getOneUser = async (id) => {
+  try {
+    const user = await User.findOne({ userID: id });
+    return user;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-// // DELETE ONE BY USER_ID FUNCTION
+// UPDATE ONE BY USER_ID function
+const updateOneUser = async (id, updateData) => {
+  try {
+    const updatedUser = await User.findOneAndUpdate(id, updateData, {
+      new: true,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-// module.exports = { User, createNewUser, getAll, getOneUser };
+// DELETE ONE BY USER_ID FUNCTION
+const deleteOneUser = async (id) => {
+  try {
+    const deletedUser = await User.findOneAndDelete({ userID: id });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Export
+module.exports = {
+  createNewUser,
+  getAllUsers,
+  getOneUser,
+  updateOneUser,
+  deleteOneUser,
+};
